5516e34c37e61f93b9d643e258df751e03d7656a
송형근 <soo1095@naver.com>
2024-07-08 16:48:53 +0900

    memo project Create Read

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,35 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+        Memo memo = new Memo(requestDto);
+
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        memoList.put(maxId, memo);
+
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        return memoResponseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos(){
+        List<MemoResponseDto> responseList = memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+        return responseList;
+    }
+}
