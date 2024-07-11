653c7af5d15c0eb1b93d920ca17e2e3fb9275961
송형근 <soo1095@naver.com>
2024-07-11 15:03:09 +0900

    fixed untracked files

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -6,7 +6,6 @@ import jakarta.persistence.*;
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // nullable: null 허용 여부
@@ -17,4 +16,16 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
